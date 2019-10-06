526866200e80e40ff8cb5dc659b1244ee47df9d0
Marcin Dubel <marcin@appsilon.com>
2019-10-06 11:05:38 +0200

    Saved questions and answers

---
+++ app/logic/questions.R
@@ -0,0 +1,26 @@
+QUESTIONS <- list(
+  question1 = "Zakres kompetencji samorządów terytorialnych należy stopniowo poszerzać.",
+  question2 = "Siedziby części urzędów centralnych należy przenieść poza Warszawę, do innych miast w całym kraju.",
+  question3 = "Należy wzmocnić niezależność władzy sądowniczej od parlamentu i rządu.",
+  question4 = "Wiek emerytalny powinien zostać podwyższony.",
+  question5 = "Kwota wolna od podatku dochodowego powinna zostać znacząco podniesiona.",
+  question6 = "Pacjent w publicznej placówce systemu opieki zdrowotnej powinien mieć możliwość dopłacenia do wyższego standardu świadczeń medycznych (np. protezy z lepszego materiału).",
+  question7 = "Należy ograniczyć zakres wiedzy teoretycznej nauczanej w szkołach na rzecz kształcenia umiejętności uczniów.",
+  question8 = "Priorytetem polityki kulturalnej państwa powinno być wzmacnianie tożsamości narodowej.",
+  question9 = "Środki publiczne na naukę powinny być skoncentrowane na wspieraniu najlepszych uczelni w kraju.",
+  question10 = "Media informacyjne powinny pozostawać pod kontrolą polskiego kapitału.",
+  question11 = "Chrześcijańskie wartości powinny być podstawą polityki społecznej państwa.",
+  question12 = "Ustawa dotycząca aborcji powinna zostać złagodzona.",
+  question13 = "Osoby tej samej płci powinny mieć możliwość zawarcia związku partnerskiego.",
+  question14 = "Węgiel powinien pozostać podstawowym źródłem energii w Polsce.",
+  question15 = "Polska powinna przyspieszyć budowę elektrowni atomowej.",
+  question16 = "Należy kontynuować rozwijanie i wzmacnianie Wojsk Obrony Terytorialnej.",
+  question17 = "Polska powinna przyjmować większą liczbę imigrantów zarobkowych z innych krajów.",
+  question18 = "Unia Europejska powinna mieć mniejszy wpływ na polską politykę wewnętrzną.",
+  question19 = "Polska powinna wspierać pogłębienie integracji europejskiej w dziedzinie polityki zagranicznej i obronnej.",
+  question20 = "Polska powinna zabiegać o utrzymanie międzynarodowych sankcji nałożonych na Rosję po agresji na Ukrainę."
+)
+
+MD <- c(1, 1,	1, 1, -1,	-1,	1, -1, -1, -1, 1, -1,	1, -1, 1, -1,	1, -1, 1, 1)
+
+ANSWERS <- read.csv2("./app/logic/answers.csv")
